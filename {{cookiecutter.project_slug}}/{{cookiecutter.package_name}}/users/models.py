9d46edb4d0d3377fc6937a3f64659bdd612e11bb
Dan Jacob <danjac2018@gmail.com>
2026-03-05 15:20:23 +0200

    feat: remove the email notifications field

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/users/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class User(AbstractUser):
     """Custom User model."""
 
-    send_email_notifications = models.BooleanField(default=True)
-
     @property
     def name(self) -> str:
         """Return the user's first name or username."""
